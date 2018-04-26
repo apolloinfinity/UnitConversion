1f526fac1f66d575ca6e86b00253b5a2cebc9f26
Javier Ramirez <apollo.infinity88@gmail.com>
2018-04-26 10:34:05 -0700

    Made some changes to the UI, refactoring code using Moduler Design Pattern

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
-document.getElementById("output").style.visibility = "hidden";
+// document.getElementById("output").style.visibility = "hidden";
+// init();
+// document.getElementById("lbsInput").addEventListener("input", function(e) {
+//   document.getElementById("output").style.visibility = "visible";
+//   let lbs = e.target.value;
+//   document.getElementById("gramsOutput").innerHTML = 
+//   document.getElementById("kgOutput").innerHTML = 
+//   document.getElementById("ozOutput").innerHTML = lbs * 16;
+// });
 
-document.getElementById("lbsInput").addEventListener("input", function(e) {
-  document.getElementById("output").style.visibility = "visible";
-  let lbs = e.target.value;
-  document.getElementById("gramsOutput").innerHTML = (lbs / 0.022046).toFixed(
-    2
-  );
-  document.getElementById("kgOutput").innerHTML = (lbs / 2.2046).toFixed(2);
-  document.getElementById("ozOutput").innerHTML = lbs * 16;
-});
+
+// function massConversion(){
+//   let lbsToGrams = (lbs / 0.022046).toFixed(2);
+//   let lbsToKilos = (lbs / 2.2046).toFixed(2);
+//   let lbsToOz = lbs * 16;
+// }
+
+// Using the Module Design Pattern
+var UIController = (function(){
+  
+})();
+
+// Controller that glues everything together.
+var controller = (function(){
+  var test = function(){
+    document.querySelector
+  }
+})();
