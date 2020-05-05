1510ac2883114d19ad48f33b5e705cf2554593d4
Javier Ramirez <apollo.infinity88@gmail.com>
2020-05-05 11:19:54 -0700

    Saving current state of project to git

--- app.js
+++ app.js
@@ -7,7 +7,6 @@
 //   document.getElementById("ozOutput").innerHTML = lbs * 16;
 // });
 
-
 /*
 Formulas for conversions.
 Centimeters = feet * 30.48
@@ -20,127 +19,123 @@ Kilo Meters
 var Conversion;
 
 const UIController = (function () {
+	const OutputStrings = {
+		inputNum: '#UnitInput',
+		outputCent: '#centOutput',
+		outputKilos: '#kiloOutput',
+		outputImperial: '#imperialOutput',
+		outputPane: '#output'
+	};
 
-  const OutputStrings = {
-    inputNum: "#UnitInput",
-    outputCent: "#centOutput",
-    outputKilos: "#kiloOutput",
-    outputImperial: "#imperialOutput",
-    outputPane: "#output"
-  };
+	const UnitStrings = {
+		mass: '#mass',
+		length: '#length',
+		volume: '#volume'
+	};
 
-  const UnitStrings = {
-    mass: "#mass",
-    length: "#length",
-    volume: "#volume"
-  }
+	const UnitNames = {
+		grams: 'Gram(s): ',
+		kgrams: 'Kilogram(s): ',
+		oz: 'Ounce(s): ',
+		cmeter: 'Centimeter(s): ',
+		meters: 'Meter(s): ',
+		kmeters: 'Kilometer(s): ',
+		inches: 'Inch(es): ',
+		mliters: 'Milliter(s): ',
+		liters: 'Liter(s): ',
+		quarts: 'Quart(s): '
+	};
 
-  const UnitNames = {
-    grams: "Gram(s): ",
-    kgrams: "Kilogram(s): ",
-    oz: "Ounce(s): ",
-    cmeter: "Centimeter(s): ",
-    meters: "Meter(s): ",
-    kmeters: "Kilometer(s): ",
-    inches: "Inch(es): ",
-    mliters: "Milliter(s): ",
-    liters: "Liter(s): ",
-    quarts: "Quart(s): "
-  }
-
-  return {
-    getDOMStrings: function () {
-      return OutputStrings;
-    },
-    getUnitStrings: function () {
-      return UnitStrings;
-    },
-    getUnitNames: function () {
-      return UnitNames;
-    }
-  };
+	return {
+		getDOMStrings: function () {
+			return OutputStrings;
+		},
+		getUnitStrings: function () {
+			return UnitStrings;
+		},
+		getUnitNames: function () {
+			return UnitNames;
+		}
+	};
 })();
 
 // For testing
 // UIController.getInput();
 
 const controller = (function (UICtrl) {
-  const setUpListners = function () {
-    // Acces object inside of UIController
-    const DOM = UIController.getDOMStrings();
-    const Unit = UIController.getUnitStrings();
-    const UnitNames = UIController.getUnitNames();
-    document.querySelector(DOM.outputPane).style.visibility = "hidden";
+	const setUpListners = function () {
+		// Acces object inside of UIController
+		const DOM = UIController.getDOMStrings();
+		const Unit = UIController.getUnitStrings();
+		const UnitNames = UIController.getUnitNames();
+		document.querySelector(DOM.outputPane).style.visibility = 'hidden';
 
-    const massSelect = document.querySelector(Unit.mass).addEventListener('click', function () {
-      document.querySelector(DOM.outputPane).style.visibility = "visible";
+		const massSelect = document.querySelector(Unit.mass).addEventListener('click', function () {
+			document.querySelector(DOM.outputPane).style.visibility = 'visible';
 
-      const grams = document.querySelector(DOM.outputCent).innerHTML = UnitNames.grams;
-      const kgrams = document.querySelector(DOM.outputKilos).innerHTML = UnitNames.kgrams;
-      const ounces = document.querySelector(DOM.outputImperial).innerHTML = UnitNames.oz;
+			const grams = (document.querySelector(DOM.outputCent).innerHTML = UnitNames.grams);
+			const kgrams = (document.querySelector(DOM.outputKilos).innerHTML = UnitNames.kgrams);
+			const ounces = (document.querySelector(DOM.outputImperial).innerHTML = UnitNames.oz);
 
+			document.querySelector(DOM.inputNum).addEventListener('input', function (e) {
+				// document.querySelector(DOM.outputCent).style.visibility = "visible";
+				let lbs = e.target.value;
+				document.querySelector(DOM.outputCent).innerHTML = grams + (lbs / 0.00220462).toFixed(2);
+				document.querySelector(DOM.outputKilos).innerHTML = kgrams + (lbs / 2.2046).toFixed(6);
+				document.querySelector(DOM.outputImperial).innerHTML = ounces + (lbs * 16).toFixed(2);
+			});
 
-      document.querySelector(DOM.inputNum).addEventListener("input", function (e) {
-        // document.querySelector(DOM.outputCent).style.visibility = "visible";
-        let lbs = e.target.value;
-        document.querySelector(DOM.outputCent).innerHTML = grams + (lbs / 0.00220462).toFixed(2);
-        document.querySelector(DOM.outputKilos).innerHTML = kgrams + (lbs / 2.2046).toFixed(6);
-        document.querySelector(DOM.outputImperial).innerHTML = ounces + (lbs * 16).toFixed(2);
-      });
+			console.log('Clicked mass button');
+		});
 
-      console.log("Clicked mass button");
-    })
+		const lengthSelect = document.querySelector(Unit.length).addEventListener('click', function () {
+			document.querySelector(DOM.outputPane).style.visibility = 'visible';
 
-    const lengthSelect = document.querySelector(Unit.length).addEventListener('click', function () {
-      document.querySelector(DOM.outputPane).style.visibility = "visible";
+			const centimeter = (document.querySelector(DOM.outputCent).innerHTML = UnitNames.cmeter);
+			const kilometer = (document.querySelector(DOM.outputKilos).innerHTML = UnitNames.kmeters);
+			const inches = (document.querySelector(DOM.outputImperial).innerHTML = UnitNames.inches);
 
-      const centimeter = document.querySelector(DOM.outputCent).innerHTML = UnitNames.cmeter;
-      const kilometer = document.querySelector(DOM.outputKilos).innerHTML = UnitNames.kmeters;
-      const inches = document.querySelector(DOM.outputImperial).innerHTML = UnitNames.inches;
+			document.querySelector(DOM.inputNum).addEventListener('input', function (e) {
+				// document.querySelector(DOM.outputCent).style.visibility = "visible";
+				let feet = e.target.value;
+				document.querySelector(DOM.outputCent).innerHTML = centimeter + (feet * 30.48).toFixed(2);
+				document.querySelector(DOM.outputKilos).innerHTML = kilometer + (feet / 3280.8).toFixed(6);
+				document.querySelector(DOM.outputImperial).innerHTML = inches + (feet * 12).toFixed(2);
+			});
 
-      document.querySelector(DOM.inputNum).addEventListener("input", function (e) {
-        // document.querySelector(DOM.outputCent).style.visibility = "visible";
-        let feet = e.target.value;
-        document.querySelector(DOM.outputCent).innerHTML = centimeter + (feet * 30.48).toFixed(2);
-        document.querySelector(DOM.outputKilos).innerHTML = kilometer + (feet / 3280.8).toFixed(6);
-        document.querySelector(DOM.outputImperial).innerHTML = inches + (feet * 12).toFixed(2);
-      });
+			console.log('Clicked length button');
+		});
 
-      console.log("Clicked length button");
-    })
+		const volumeSelect = document.querySelector(Unit.volume).addEventListener('click', function () {
+			document.querySelector(DOM.outputPane).style.visibility = 'visible';
 
-    const volumeSelect = document.querySelector(Unit.volume).addEventListener('click', function () {
-      document.querySelector(DOM.outputPane).style.visibility = "visible";
+			const milliters = (document.querySelector(DOM.outputCent).innerHTML = UnitNames.mliters);
+			const liters = (document.querySelector(DOM.outputKilos).innerHTML = UnitNames.liters);
+			const quarts = (document.querySelector(DOM.outputImperial).innerHTML = UnitNames.quarts);
 
-      const milliters = document.querySelector(DOM.outputCent).innerHTML = UnitNames.mliters;
-      const liters = document.querySelector(DOM.outputKilos).innerHTML = UnitNames.liters;
-      const quarts = document.querySelector(DOM.outputImperial).innerHTML = UnitNames.quarts;
+			document.querySelector(DOM.inputNum).addEventListener('input', function (e) {
+				// document.querySelector(DOM.outputCent).style.visibility = "visible";
+				let gallons = e.target.value;
+				document.querySelector(DOM.outputCent).innerHTML = milliters + (gallons * 3785.411784).toFixed(2);
+				document.querySelector(DOM.outputKilos).innerHTML = liters + (gallons * 3.78541178).toFixed(6);
+				document.querySelector(DOM.outputImperial).innerHTML = quarts + (gallons * 4).toFixed(2);
+			});
 
-      document.querySelector(DOM.inputNum).addEventListener("input", function (e) {
-        // document.querySelector(DOM.outputCent).style.visibility = "visible";
-        let gallons = e.target.value;
-        document.querySelector(DOM.outputCent).innerHTML = milliters + (gallons * 3785.411784).toFixed(2);
-        document.querySelector(DOM.outputKilos).innerHTML = liters + (gallons * 3.78541178).toFixed(6);
-        document.querySelector(DOM.outputImperial).innerHTML =quarts + (gallons * 4).toFixed(2);
-      });
+			console.log('Clicked volume button');
+		});
+	};
 
-      console.log("Clicked volume button");
-    })
-
-    
-  };
-
-  // const ctrlUpdate = function(){ 
-  //   const input = UICtrl.getInput();
-  //   console.log(input)
-  // }
-  return {
-    init: function () {
-      console.log("App started.");
-      setUpListners();
-      // ctrlUpdate();
-    }
-  };
+	// const ctrlUpdate = function(){
+	//   const input = UICtrl.getInput();
+	//   console.log(input)
+	// }
+	return {
+		init: function () {
+			console.log('App started.');
+			setUpListners();
+			// ctrlUpdate();
+		}
+	};
 })(UIController);
 
 controller.init();
